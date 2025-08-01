188ad62ea8a4161fd6c5eb7a217f352e80f2e6a0
Alejaguisadocortes <aleja9903@hotmail.com>
2025-07-31 22:11:29 -0500

    Add files via upload

---
+++ app.config.js
@@ -0,0 +1,9 @@
+<configuration>
+  <appSettings>
+    <add key="NombreUsuario" value="UsuarioPredeterminado" />
+    <add key="MaxIntentos" value="3" />
+  </appSettings>
+  <connectionStrings>
+    <add name="MiConexion" connectionString="Server=localhost;Database=MiBaseDeDatos;User Id=usuario;Password=contraseña;" providerName="System.Data.SqlClient" />
+  </connectionStrings>
+</configuration>
