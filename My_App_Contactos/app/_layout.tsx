2fdeb5689093090f640b9083e8f9a543be9eb2b1
Alejandra <aleja9903@hotmail.com>
2025-08-07 17:51:30 -0500

    Completando

--- My_App_Contactos/app/_layout.tsx
+++ My_App_Contactos/app/_layout.tsx
@@ -1,6 +1,6 @@
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { Slot } from 'expo-router';
 import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
 
@@ -13,16 +13,12 @@ export default function RootLayout() {
   });
 
   if (!loaded) {
-    // Async font loading only occurs in development.
     return null;
   }
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
+      <Slot /> {/* Renderiza las rutas hijas como tabs o stacks */}
       <StatusBar style="auto" />
     </ThemeProvider>
   );
